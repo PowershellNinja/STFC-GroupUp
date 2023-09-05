4e930cb28f27c636e4166ed3c73f60379640b989
PowershellNinja <raphael.faeh@corporatesoftware.ch>
2023-09-05 16:17:36 +0200

    Enable rest debug.

--- mod.ts
+++ mod.ts
@@ -3,6 +3,8 @@ import { DEBUG, LOCALMODE } from './flags.ts';
 import { createBot, enableCachePlugin, enableCacheSweepers, initLog, Intents, startBot } from './deps.ts';
 import { events } from './src/events.ts';
 import { createSlashCommands } from './src/commands/_index.ts';
+import { tokenMap } from './src/buttons/tokenCleanup.ts';
+import utils from './src/utils.ts';
 
 // Initialize logging client with folder to use for logs, needs --allow-write set on Deno startup
 initLog('logs', DEBUG);
@@ -12,6 +14,9 @@ const bot = enableCachePlugin(createBot({
 	token: LOCALMODE ? config.localToken : config.token,
 	intents: Intents.MessageContent | Intents.GuildMessages | Intents.DirectMessages | Intents.Guilds | Intents.GuildMessageReactions,
 	events,
+	rest: {
+		debug: (debugStr: string) => utils.commonLoggers.logMessage('REST-Debug', debugStr),
+	  },
 }));
 enableCacheSweepers(bot);
 
